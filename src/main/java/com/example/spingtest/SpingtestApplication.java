d85ea887c3103dae35744ddc82d8c2074904cfac
ChienTQ2003 <anhchienbs.1@gmail.com>
2023-11-08 21:36:13 +0700

    first commit

---
+++ src/main/java/com/example/spingtest/SpingtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.spingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spingtest/SpingtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
